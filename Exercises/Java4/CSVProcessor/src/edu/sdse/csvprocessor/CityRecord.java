2c9e4bc205cf2bd1863efd5b47630762716df19f
Sabrina Pereira <sabrinafp35@gmail.com>
2022-10-12 08:55:02 +0200

    Added getters.

--- Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityRecord.java
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityRecord.java
@@ -2,20 +2,39 @@ package edu.sdse.csvprocessor;
 
 public class CityRecord {
 
-  int id;
-  int year;
-  String city;
-  int population;
+    private int id;
+    private int year;
+    private String city;
+    private int population;
 
-  public CityRecord(int id, int year, String city, int population) {
-    this.id = id;
-    this.year = year;
-    this.city = city;
-    this.population = population;
-  }
+    public CityRecord(int id, int year, String city, int population) {
+        this.id = id;
+        this.year = year;
+        this.city = city;
+        this.population = population;
+    }
 
-  @Override
-  public String toString() {
-      return "id: " + id + ", year: " + year + ", city: " + city + ", population: " + population;
-  }
+    // getters
+    public int getId() {
+        return id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getPop() {
+        return population;
+    }
+
+    // override toString so it doesn't print the hash of the object, but it's
+    // contents
+    @Override
+    public String toString() {
+        return "id: " + id + ", year: " + year + ", city: " + city + ", population: " + population;
+    }
 }
