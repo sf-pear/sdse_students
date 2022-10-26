f2e61ecd76b5cfe709c7e57df23ddc06635009ba
Sabrina Pereira <sabrinafp35@gmail.com>
2022-10-26 17:17:39 +0200

    Added article class.

--- Project 1/Phase 1/Code/Phase 1/src/org/nypl/journalsystem/Journal.java
+++ Project 1/Phase 1/Code/Phase 1/src/org/nypl/journalsystem/Journal.java
@@ -2,22 +2,37 @@ package org.nypl.journalsystem;
 import java.util.*;
 
 class Journal {
-    String journalName;
-    String journalIssn;
-    ArrayList<String> journalArticles;
-    int journalCount = 0;
+    String name;
+    String issn;
+    ArrayList<String> articles;
+    // int journalCount = 0;
 
-    public Journal(String journalName, String journalIssn, ArrayList<String> journalArticles) {
-        this.journalName = journalName;
-        this.journalIssn = journalIssn;
-        this.journalArticles = journalArticles;
+    public Journal(String name, String issn, ArrayList<String> articles) {
+        this.name = name;
+        this.issn = issn;
+        this.articles = articles;
     }
 
     // override toString so it doesn't print the hash of the object, but it's
     // contents
-    @Override
-    public String toString() {
-        return "id: " + id + ", year: " + year + ", city: " + city + ", population: " + population;
+    // @Override
+    // public String toString() {
+    //     return "id: " + id + ", year: " + year + ", city: " + city + ", population: " + population;
+    // }
+    
+}
+
+class Article {
+    int id;
+    String title;
+    ArrayList<Integer> authors; // int not allowed - why cant have authors object stored here?
+    String journalIssn; // how do i link these without storing the information twice?
+
+    public Article(int id, String title, ArrayList<Integer> authors, String journalIssn) {
+        this.id = id;
+        this.title = title;
+        this.authors = authors;
+        this.journalIssn = journalIssn;
     }
     
 }
